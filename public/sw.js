3d82ac3a0716f51369f3453b835693b50de877d1
Gabriel Vargas <gabrielvargas.metan@gmail.com>
2026-09-17 07:17:59 -0300

    feat: add CRV4 PWA icons and offline assets

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,16 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/'])));
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll([
+    '/',
+    '/styles.css',
+    '/app.js',
+    '/manifest.webmanifest',
+    '/icons/crv4-192.png',
+    '/icons/crv4-512.png',
+    '/icons/favicon-32.png',
+    '/icons/apple-touch-icon.png',
+  ])));
 });
 
 self.addEventListener('fetch', (event) => {
