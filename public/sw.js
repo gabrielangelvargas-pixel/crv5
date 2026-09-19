c8031b250c67586fbce275012091a0618cf25df0
Gabriel Vargas <gabrielvargas.metan@gmail.com>
2026-09-18 23:53:54 -0300

    style: reforzar footer y compactar ofertas

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'crv5-shell-v14';
+const CACHE_NAME = 'crv5-shell-v15';
 const NETWORK_FIRST_PATHS = new Set(['/', '/app.js', '/styles.css', '/manifest.webmanifest', '/rubros', '/catalogo', '/login']);
 
 self.addEventListener('activate', (event) => {
